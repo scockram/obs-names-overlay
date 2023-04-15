90f99ebdc97200213eb341230b3485b723ecd932
Steve Cockram <stevejohncockram@gmail.com>
2023-04-15 16:38:22 +0100

    port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 import WebSocket, { WebSocketServer } from 'ws';
 
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({ port: process.env.PORT || 3000 });
 
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
